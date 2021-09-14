6c7c6b97f93a33f3dc0ad12217311c44e7f24816
ANgajasinghe <angajasinghe@gmail.com>
2021-09-14 16:49:23 +0530

    Add project files.

---
+++ TaskApp.Test/TasksTest.cs
@@ -0,0 +1,43 @@
+﻿using MediatR;
+using Moq;
+using Xunit;
+
+namespace TaskApp.Test;
+
+public class TasksTest
+{
+
+    // I mock a mediator and it will be the global object since I am using XUnit 
+    // This type of mocking cannot get expected result we do not know our code is executed successfully or data gave been committed in to database really.
+    // That why we need intergeneration testing.
+    // But using these type tests we can check our logics
+
+    Mock<IMediator> _mediatorMock = new();
+
+    [Fact]
+    public async Task CreateATask()
+    {
+        // Arrange
+        CreateTaskCommand command = new()
+        {
+            Id = 1,
+            Email = "ag.anayanajith@gmail.com",
+            Name = "Some task",
+            DueDate = DateTime.Now.AddDays(1),
+            Priority = 1,
+            IsCompleated = false
+        };
+
+        _mediatorMock.Setup(m => m.Send(It.IsAny<CreateTaskCommand>(), It.IsAny<CancellationToken>())).ReturnsAsync(1);
+
+        // Act
+        var result = await _mediatorMock.Object.Send(command);
+
+
+        Assert.Equal(1, result);
+
+        _mediatorMock.Verify(x => x.Send(It.IsAny<CreateTaskCommand>(), It.IsAny<CancellationToken>()));
+
+    }
+}
+
---
+++ TaskApp/Services/Tasks/Commands/CreateTaskCommand.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+
+namespace TaskApp.Test
+{
+    public class CreateTaskCommand : IRequest<int>
+    {
+        public int Id { get; set; }
+        public string? Email { get; set; }
+        public string? Name { get; set; }
+        public DateTime DueDate { get; set; }
+        public int Priority { get; set; }
+        public bool IsCompleated { get; set; }
+    }
+
+
+    public class CreateTaskCommandHandler : IRequestHandler<CreateTaskCommand, int>
+    {
+
+
+        public async Task<int> Handle(CreateTaskCommand request, CancellationToken cancellationToken)
+        {
+            if (request.Email != null)
+            {
+                return 1;
+            }
+
+            return 0;
+        }
+    }
+}
